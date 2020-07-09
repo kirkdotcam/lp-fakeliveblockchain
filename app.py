2a8d4e900e841f204b233fe7e342679cb59d13c9
CamKirk <camden.kirkland@gmail.com>
2020-07-09 04:02:55 -0500

    messages broadcast

--- app.py
+++ app.py
@@ -2,23 +2,38 @@ from flask import Flask, render_template, jsonify
 
 from flask_socketio import SocketIO, emit
 import rsa
-
+import binascii
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+current_users = []
 
 
-def keyrequest():
-    (public,private) = rsa.newkeys(512)
-    public = public._save_pkcs1_pem().decode("utf-8").split("\n")[1]
-    private = private._save_pkcs1_pem().decode("utf-8").split("\n")[1]
+def keyrequest(name="anonymous"):
+    (public_ref,private_ref) = rsa.newkeys(512)
+    public = public_ref._save_pkcs1_pem().decode("utf-8").split("\n")[1]
+    private = private_ref._save_pkcs1_pem().decode("utf-8").split("\n")[1]
     
-    return {
+
+    payload = {
         "public":public,
-        "private":private
+        "private":private,
+        "username":name
     }
 
+    user = {
+        "pub_reference":public_ref,
+        "priv_reference": private_ref,
+        "user": public,
+        "name": name
+    }
+    current_users.append(user)
+
+    return payload
+
+def find_user(userpublicpem):
+    return list(filter(lambda x: x["user"],current_users))[0]
 
 @app.route("/")
 def index():
@@ -29,11 +44,47 @@ def index():
 def socket_connection():
     print("new connection")
 
+@socketio.on("disconnect")
+def socket_disconnection(data):
+    print("")
+
 @socketio.on("newkeys")
 def socket_newkeys():
     emit("newkeys",keyrequest(), json=True)
 
+@socketio.on("decrypt")
+def socket_decrypt(data):
+    pass
+
+
+
+@socketio.on("message")
+def socket_message(data):
+
+    
+    try:
+
+
+        recipient = find_user(data["to"])
+        textset_msg = data["msg"].encode("utf-8")
+   
+        enc_msg = rsa.encrypt(textset_msg,recipient["pub_reference"])
+        ascii_msg = binascii.b2a_base64(enc_msg).decode("utf-8")
+
+        payload = {
+            "message":ascii_msg,
+            "to":data["to"]
+        }
+
+        # print(data["msg"])
+        emit("newmessage",payload, broadcast=True)
+        
+    except:
+
+        emit("messageError")
+
+
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0")
+    socketio.run(app, host="0.0.0.0", debug=True)
--- static/main.js
+++ static/main.js
@@ -1,33 +1,71 @@
 let socket = io();
 let userInfo = {
-    "public":"",
-    "private":""
+    "public": "",
+    "private": "",
+    "username": "Anonymous"
 };
 
-socket.on("connect", ()=>{
+socket.on("connect", () => {
     console.log("connected!");
 
     // request new keys on connection
-    socket.emit("newkeys");
-    
+    newKeys();
+
 });
 
-socket.on("newkeys", (data,cb)=>{
+socket.on("disconnect",userInfo)
+
+socket.on("newkeys", (data, cb) => {
     // console.log(data);
-    Object.assign(userInfo,data);
-    console.log(userInfo);
-    
+    Object.assign(userInfo, data);
     updateKeys();
 
 });
 
-function sendmessage(msg){
+socket.on("decryptedSuccess")
+
+socket.on("decryptedFailure")
+
+socket.on("newmessage",(data)=>{
+    console.log("new message:", data)
+});
+
+socket.on("messageError", ()=>{
+    // alert("YA BLEW IT!")
+})
+
+function sendmessage(msg, to) {
     // sends message to the chain
-    console.log("pop")
+    msg = msg ? msg : document.getElementById("messageText").value;
+    to = to ? to : document.getElementById("messageTo").value;
+
+    if (!msg) alert("need a message!");
+    if (!to) alert("who am I sending this to?");
+
+    let payload = {
+        msg,
+        to,
+        from: userInfo.public
+    }
+
+    socket.emit("message",payload);
 };
 
-function updateKeys(){
+function updateKeys() {
     // updates visible keys on page
     document.getElementById("userpublic").textContent = userInfo.public;
     document.getElementById("userprivate").textContent = userInfo.private;
+
 };
+
+function newKeys() {
+    socket.emit("newkeys")
+}
+
+function renderMessages() {
+
+}
+
+function decrypt() {
+    socket.emit("decrypt")
+}
