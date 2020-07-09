c1be3666ef31d58d577169011353e21b1dabd002
CamKirk <camden.kirkland@gmail.com>
2020-07-09 01:27:28 -0500

    working private/public on frontend

--- static/main.js
+++ static/main.js
@@ -1,13 +1,33 @@
-let socket = io()
+let socket = io();
+let userInfo = {
+    "public":"",
+    "private":""
+};
 
 socket.on("connect", ()=>{
     console.log("connected!");
 
+    // request new keys on connection
     socket.emit("newkeys");
     
 });
 
 socket.on("newkeys", (data,cb)=>{
-    console.log(data);
+    // console.log(data);
+    Object.assign(userInfo,data);
+    console.log(userInfo);
     
-})
+    updateKeys();
+
+});
+
+function sendmessage(msg){
+    // sends message to the chain
+    console.log("pop")
+};
+
+function updateKeys(){
+    // updates visible keys on page
+    document.getElementById("userpublic").textContent = userInfo.public;
+    document.getElementById("userprivate").textContent = userInfo.private;
+};
